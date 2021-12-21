c625c2df279b46b4fe6fddd2eaceb213a392c3f4
Ariel Ramírez <1mundo.it@gmail.com>
2021-12-20 22:43:26 -0600

    se cambia dependencia

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,7 @@
     'summary': 'A new app to manage a Hospital',
     'version': '12.0.0',
     'description': """This module adds a Twitter roller building block to the website builder, so that you can display Twitter feeds on any page of your website.""",
-    'depends': ['website'],
+    'depends': ['sale'],
     'data': [
         'security/ir.model.access.csv',
         'data/website_twitter_data.xml',
