31ecadfb18ea763a4d91fbe6d9a4211cfd738b83
Ariel Ramírez <1mundo.it@gmail.com>
2021-12-20 23:21:02 -0600

    se corrige un error en manifest

--- __manifest__.py
+++ __manifest__.py
@@ -8,12 +8,7 @@
     'version': '12.0.0',
     'description': """This module adds a Twitter roller building block to the website builder, so that you can display Twitter feeds on any page of your website.""",
     'depends': ['contacts'],
-    'data': [
-        'security/ir.model.access.csv',
-        'data/website_twitter_data.xml',
-        'views/res_config_settings_views.xml',
-        'views/website_twitter_snippet_templates.xml'
-    ],
+    'data': [],
     'installable': True,
     'license': 'LGPL-3',
     'application' : True,
