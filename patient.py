e0f8208e1335bf7c10e53a0ea0ad8502a9b09e94
Ariel Ramírez <1mundo.it@gmail.com>
2021-12-20 23:18:28 -0600

    Agregar un modelo

---
+++ patient.py
@@ -0,0 +1,11 @@
+from typing_extensions import Required
+from odoo import models, fields
+
+class HospitalPatient(models.Model):
+    _name = "hospital.patient" 
+    _description = "Patient Record"
+
+    patient_name = fields.Char(string='Name', required=True)
+    patient_age = fields.Integer(string='Age')
+    notes = fields.Text(string='Notes')
+    image = fields.Binary(string ='Image')
